493b5b0efbec47768bde14db834959f6a0516c71
AshokGummanoor <gummanoorashok@gmail.com>
2023-01-05 04:13:07 +0000

    Changes are made for Intercative staging

--- cal.sh
+++ cal.sh
@@ -6,6 +6,7 @@ echo "1. add"
 echo "2. sub"
 echo "3. Divison"
 echo "4. Multiply"
+echo "5. none"
 read choice
 
 case $choice in
@@ -16,3 +17,4 @@ case $choice in
 esac
 
 echo "Result is :$res"
+echo "None is printed"
